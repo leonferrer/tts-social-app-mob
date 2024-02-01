20e3cd5f32b18130ce0edcd8bac9284ef8de142b
Leonardo Ferreira <leonferrer@gmail.com>
2024-02-01 11:36:31 -0300

    firstdeploy

--- App.tsx
+++ App.tsx
@@ -7,6 +7,29 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAOrJP0juWYN4rXMYkOe4aeO7cysA0n4y4",
+  authDomain: "tts-social-auth.firebaseapp.com",
+  projectId: "tts-social-auth",
+  storageBucket: "tts-social-auth.appspot.com",
+  messagingSenderId: "627783999636",
+  appId: "1:627783999636:web:863193a342aaf189d1d481",
+  measurementId: "G-630YXDVG6Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 import {
   SafeAreaView,
   ScrollView,
